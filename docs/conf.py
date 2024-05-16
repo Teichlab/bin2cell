961ff52c0192f358e8338c8fed869626688688ad
ktpolanski <krzysztof_polanski@o2.pl>
2024-05-16 12:54:29 +0000

    RTD attempt

---
+++ docs/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Bin2cell'
+copyright = '2024, Krzysztof Polanski, Nadav Yayon'
+author = 'Krzysztof Polanski, Nadav Yayon'
+import bin2cell as b2c
+release = b2c.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'splinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,36 @@
+.. Bin2cell documentation master file, created by
+   sphinx-quickstart on Thu May 16 12:40:32 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Bin2cell
+========
+
+Project home page `here <https://github.com/Teichlab/bin2cell>`_.
+
+Main workflow functions
+-----------------------
+.. autosummary::
+   :toctree:
+   
+   bin2cell.scaled_he_image
+   bin2cell.destripe
+   bin2cell.grid_image
+   bin2cell.stardist
+   bin2cell.insert_labels
+   bin2cell.expand_labels
+   bin2cell.salvage_secondary_labels
+   bin2cell.bin_to_cell
+
+Utility functions
+-----------------
+.. autosummary::
+   :toctree:
+   
+   bin2cell.get_crop
+   bin2cell.view_stardist_labels
+   bin2cell.load_image
+   bin2cell.check_array_coordinates
+   bin2cell.mpp_to_scalef
+   bin2cell.get_mpp_coords
+   bin2cell.destripe_counts
