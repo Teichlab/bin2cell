fe42576dfbc38ce4f4d7c3e351dcc8f00f01139f
ktpolanski <krzysztof_polanski@o2.pl>
2024-05-16 12:58:53 +0000

    theme typo

--- docs/conf.py
+++ docs/conf.py
@@ -25,5 +25,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'splinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
