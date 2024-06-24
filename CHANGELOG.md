e2043d608bbadfa12df2e4e7e7021f2c0a5cc03c
ktpolanski <krzysztof_polanski@o2.pl>
2024-06-24 13:21:02 +0000

    0.1.1

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## 0.1.1
+- cap `numpy` at 1.x for the time being
+
+## 0.1.0
+- initial release
--- bin2cell/__init__.py
+++ bin2cell/__init__.py
@@ -1,4 +1,4 @@
 """Join subcellular Visium HD bins into cells"""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from .bin2cell import *
